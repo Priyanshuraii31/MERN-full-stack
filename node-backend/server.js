e97d3b7c5350fff30c640082f384ce0035796477
Priyanshu Rai <priyanshurai9352@gmail.com>
2026-03-03 15:57:55 +0530

    updated env

--- node-backend/server.js
+++ node-backend/server.js
@@ -2,25 +2,23 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors"); 
-
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 app.use(cors());
-
-// Middleware
 app.use(express.json());
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URL)
+console.log("ENV VALUE:", process.env.MONGO_URL);
 
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB Connection Error:", err));
+// ✅ Simple connect (no extra options)
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log("MongoDB Connection Error ❌:", err));
 
-// Basic Routes
 app.get("/", (req, res) => {
   res.send("Hello from Express Server");
 });
@@ -29,11 +27,9 @@ app.get("/about", (req, res) => {
   res.send("This is About Page");
 });
 
-// Use Routes
 app.use("/api", userRoutes);
 
-// Server Start
-const port = process.env.PORT ;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
