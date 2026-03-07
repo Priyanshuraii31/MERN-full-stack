35f3ac708f9330e8071b0adf332778f12c7d8377
Priyanshu Rai <priyanshurai9352@gmail.com>
2026-03-07 16:05:39 +0530

    project update

--- node-backend/server.js
+++ node-backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -7,13 +8,30 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-// database connect
 connectDB();
 
-// test route
 app.get("/", (req, res) => {
   res.send("API running 🚀");
+});
+
+app.post("/api/signup", (req, res) => {
+  const { name, email, password } = req.body;
+
+  res.json({
+    message: "Signup successful",
+    user: { name, email }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+
+  res.json({
+    message: "Login successful",
+    user: { email }
+  });
 });
 
 const PORT = process.env.PORT || 5001;
