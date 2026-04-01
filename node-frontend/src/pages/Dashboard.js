bd2c07f1fd112ce7faa1f2c71ad908dbd4ac6603
Priyanshu Rai <priyanshurai9352@gmail.com>
2026-04-01 21:50:04 +0530

    updated env

--- node-frontend/src/pages/Dashboard.js
+++ node-frontend/src/pages/Dashboard.js
@@ -1,23 +1,59 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Dashboard.css";
 
-function Dashboard() {
+const Dashboard = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        navigate("/login");
+        return;
+      }
+
+      try {
+        const res = await axios.get(
+          "https://mern-full-stack-prcf.onrender.com/api/profile",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        setUser(res.data.user);
+      } catch (error) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, [navigate]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/login");
   };
 
- return (
-  <div className="dashboard-wrapper">
-    <div className="dashboard-card">
-      <h2>Dashboard</h2>
-      <p>Welcome! You are logged in 🎉</p>
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Dashboard</h1>
+      <h2>Welcome, {user?.email}</h2>
       <button onClick={handleLogout}>Logout</button>
     </div>
-  </div>
-);
-}
+  );
+};
 
 export default Dashboard;
